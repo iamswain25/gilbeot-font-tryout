8d6387612ef1c41a579230f973d95722ac6fcfd7
swain <iamswain@hotmail.com>
2021-03-18 08:40:27 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import "normalize.css";
+import "./App.css";
+import TextareaAutosize from "react-autosize-textarea";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <section className="container">
+      <h1>
+        <span className="Gilbeot_Rainbow">길벗체 웹 테스트</span>
+        <a href="http://rainbowfoundation.co.kr/" className="Gilbeot_TG link">
+          홈페이지 가기
         </a>
-      </header>
-    </div>
+      </h1>
+      <div>
+        <h2>
+          <TextareaAutosize
+            className="Gilbeot_BI underline"
+            defaultValue="Gilbeot_BI"
+          />
+        </h2>
+        <h2>
+          <TextareaAutosize
+            className="Gilbeot_Rainbow underline"
+            defaultValue="Gilbeot_Rainbow"
+          />
+        </h2>
+        <h2>
+          <TextareaAutosize
+            className="Gilbeot_TG underline"
+            defaultValue="Gilbeot_TG"
+          />
+        </h2>
+      </div>
+    </section>
   );
 }
 
